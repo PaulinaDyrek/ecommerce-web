7573e112f23ae17efa9d28774fb5c99575b2a444
Paulina Dyrek <paoladlugosz@gmail.com>
2024-04-12 13:43:27 +0200

    update mainsection

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Lightbox></Lightbox> */}
+      <Lightbox products={products} />
       <MainSection />
     </>
   );
--- src/component/Lightbox.js
+++ src/component/Lightbox.js
@@ -4,7 +4,9 @@ import { useState } from "react";
 export default function Lightbox() {
   return (
     <>
-      <article className="bg-black bg-opacity-75 absolute top-0 left-0 bottom-0 right-0 z-50"></article>
+      <article className="bg-black bg-opacity-75 absolute top-0 left-0 bottom-0 right-0 z-50">
+        <div>{products.map}</div>
+      </article>
     </>
   );
 }
