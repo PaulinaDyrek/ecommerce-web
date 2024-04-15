11a2883509e600a002e558af869b0adbb2d5b9ca
Paulina Dyrek <paoladlugosz@gmail.com>
2024-04-15 13:53:53 +0200

    update lightbox

--- src/component/MainSection.js
+++ src/component/MainSection.js
@@ -6,6 +6,49 @@ import plus from "../images/icon-plus.svg";
 import { TiShoppingCart } from "react-icons/ti";
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
+function Lightbox({ products, slideIndex, nextSlide, previousSlide }) {
+  return (
+    <>
+      <article className="bg-black bg-opacity-75 fixed top-0 right-0 bottom-0 left-0 z-50">
+        <div className="flex items-center justify-center h-screen">
+          {products.map((item, index) => (
+            <div
+              key={index}
+              className={slideIndex === index + 1 ? "relative" : "hidden"}
+            >
+              <img
+                src={item.mainImage}
+                alt=""
+                className="lg:h-screen lg:rounded-2xl"
+                style={{
+                  height: "600px",
+                }}
+              />
+              <ul>
+                <li>
+                  <button
+                    onClick={previousSlide}
+                    className="bg-white rounded-full p-5 shadow absolute top left-4 top-1/2 -translate-y-1/2"
+                  >
+                    <FaChevronLeft />
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={nextSlide}
+                    className="bg-white rounded-full p-5 shadow absolute top right-4 top-1/2 -translate-y-1/2"
+                  >
+                    <FaChevronRight />
+                  </button>
+                </li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      </article>
+    </>
+  );
+}
 export default function MainSection() {
   const [products] = useState(data);
   const [value, setValue] = useState(0);
@@ -34,97 +77,105 @@ export default function MainSection() {
   };
 
   return (
-    <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:place-items-center lg:py-5">
-      <article>
-        <div className="lg:w-3/4">
-          {products.map((item, index) => (
-            <div
-              key={index}
-              className={slideIndex === index + 1 ? "relative" : "hidden"}
-            >
-              <img
-                src={item.mainImage}
-                alt=""
-                className="w-full lg:rounded-2xl"
-              />
-              <ul className="lg:hidden">
-                <li>
-                  <button
-                    onClick={previousSlide}
-                    className="bg-white rounded-full p-5 shadow absolute top left-4 top-1/2 -translate-y-1/2"
-                  >
-                    <FaChevronLeft />
-                  </button>
-                </li>
-                <li>
-                  <button
-                    onClick={nextSlide}
-                    className="bg-white rounded-full p-5 shadow absolute top right-4 top-1/2 -translate-y-1/2"
-                  >
-                    <FaChevronRight />
-                  </button>
-                </li>
-              </ul>
-            </div>
-          ))}
-        </div>
-        <ul className="hidden lg:flex items-center justify-start gap-5 flex-wrap mt-5">
-          {products.map((item, index) => (
-            <li
-              key={item.id}
-              onClick={() => setValue(index)}
-              className={`${
-                index === value && "border-2 border-orange-400 opacity-80"
-              } border-2 rounded-2xl overflow-hidden cursor-pointer`}
-            >
-              <img src={item.thumbnail} alt="" className="w-20 lg:w-14" />
-            </li>
-          ))}
-        </ul>
-      </article>
-      <article className="px-8 pb-10">
-        <h2 className="bg-slate-100 py-1 px-2 text-orange-400 uppercase tracking-wide text-sm font-bold inline-block rounded shadow mb-10">
-          Sneakers company
-        </h2>
-        <h1 className="text-slate-900 mb-10 font-bold text-3xl lg:text-4xl">
-          Fall Limited Edition Sneakers
-        </h1>
-        <p className="text-slate-600 mb-10 leading-relaxed">
-          These low-profile sneakers are your perfect casual wear companion.
-          Featuringa durable rubber outer sole, they'llwithstand everything the
-          weather can offer.
-        </p>
-        <div className="flex flex-wrap items-center justify-between lg:flex-col lg:items-start lg:gap-2">
-          <ul className="flex items-center gap-5">
-            <li className="text-slate-900 font-bold text-2xl">$125.00</li>
-            <li className="bg-orange-100 py-1 px-2 text-orange-400 tracking-wide text-sm font-bold inline-block rounded shadow ">
-              50%
-            </li>
-          </ul>
-          <p className="text-slate-600 text-sm">
-            <s>$250.00</s>
-          </p>
-        </div>
-        <div className="mt-10 lg:flex items-center justify-between gap-2">
-          <ul className="flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow lg:flex-1">
-            <li onClick={handleMinus} className="cursor-pointer">
-              <img src={minus} alt="" />
-            </li>
-            <li>{amount}</li>
-            <li
-              onClick={() => setAmount(amount + 1)}
-              className="cursor-pointer"
-            >
-              <img src={plus} alt="" />
-            </li>
-          </ul>
-          <div className="lg:flex-1">
-            <button className="flex items-center justify-center gap-4 bg-orange-500 py-2 px-4 text-white font-bold rounded-lg shadow mt-5 w-full lg:mt-0  hover:bg-orange-600 transition-all duration-200">
-              <TiShoppingCart /> Add to cart
-            </button>
+    <>
+      <Lightbox
+        products={products}
+        slideIndex={slideIndex}
+        nextSlide={nextSlide}
+        previousSlide={previousSlide}
+      />
+      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:place-items-center lg:py-5">
+        <article>
+          <div className="lg:w-3/4">
+            {products.map((item, index) => (
+              <div
+                key={index}
+                className={slideIndex === index + 1 ? "relative" : "hidden"}
+              >
+                <img
+                  src={item.mainImage}
+                  alt=""
+                  className="w-full lg:rounded-2xl"
+                />
+                <ul className="lg:hidden">
+                  <li>
+                    <button
+                      onClick={previousSlide}
+                      className="bg-white rounded-full p-5 shadow absolute top left-4 top-1/2 -translate-y-1/2"
+                    >
+                      <FaChevronLeft />
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={nextSlide}
+                      className="bg-white rounded-full p-5 shadow absolute top right-4 top-1/2 -translate-y-1/2"
+                    >
+                      <FaChevronRight />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ))}
           </div>
-        </div>
-      </article>
-    </section>
+          <ul className="hidden lg:flex items-center justify-start gap-5 flex-wrap mt-5">
+            {products.map((item, index) => (
+              <li
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`${
+                  index === value && "border-2 border-orange-400 opacity-80"
+                } border-2 rounded-2xl overflow-hidden cursor-pointer`}
+              >
+                <img src={item.thumbnail} alt="" className="w-20 lg:w-14" />
+              </li>
+            ))}
+          </ul>
+        </article>
+        <article className="px-8 pb-10">
+          <h2 className="bg-slate-100 py-1 px-2 text-orange-400 uppercase tracking-wide text-sm font-bold inline-block rounded shadow mb-10">
+            Sneakers company
+          </h2>
+          <h1 className="text-slate-900 mb-10 font-bold text-3xl lg:text-4xl">
+            Fall Limited Edition Sneakers
+          </h1>
+          <p className="text-slate-600 mb-10 leading-relaxed">
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuringa durable rubber outer sole, they'llwithstand everything
+            the weather can offer.
+          </p>
+          <div className="flex flex-wrap items-center justify-between lg:flex-col lg:items-start lg:gap-2">
+            <ul className="flex items-center gap-5">
+              <li className="text-slate-900 font-bold text-2xl">$125.00</li>
+              <li className="bg-orange-100 py-1 px-2 text-orange-400 tracking-wide text-sm font-bold inline-block rounded shadow ">
+                50%
+              </li>
+            </ul>
+            <p className="text-slate-600 text-sm">
+              <s>$250.00</s>
+            </p>
+          </div>
+          <div className="mt-10 lg:flex items-center justify-between gap-2">
+            <ul className="flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow lg:flex-1">
+              <li onClick={handleMinus} className="cursor-pointer">
+                <img src={minus} alt="" />
+              </li>
+              <li>{amount}</li>
+              <li
+                onClick={() => setAmount(amount + 1)}
+                className="cursor-pointer"
+              >
+                <img src={plus} alt="" />
+              </li>
+            </ul>
+            <div className="lg:flex-1">
+              <button className="flex items-center justify-center gap-4 bg-orange-500 py-2 px-4 text-white font-bold rounded-lg shadow mt-5 w-full lg:mt-0  hover:bg-orange-600 transition-all duration-200">
+                <TiShoppingCart /> Add to cart
+              </button>
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
   );
 }
